9d62c37488e5bd971973048e6f6b296eb2ae02af
Edgard Alas <edgardalasu@gmail.com>
2022-06-06 20:04:53 -0600

    fix: variables de entorno de ejemplo

--- src/middlewares/sesion.ts
+++ src/middlewares/sesion.ts
@@ -3,7 +3,7 @@ import expressSession from 'express-session';
 const unDia = 1000 * 60 * 60 * 24;
 
 export default expressSession({
-  secret: 'R8LKpk6Jz8AmG9yzg4ZDnNjM0bEfUkoWvQpkPxTdquFqgU8bWA',
+  secret: process.env.SECREY_KEY!,
   saveUninitialized: true,
   cookie: { maxAge: unDia },
   resave: false,
